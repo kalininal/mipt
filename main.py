7040df5cf698e1d5780491446a7e196da12b2705
kalinin <kalinin@povig.povig>
2024-11-08 22:30:24 +0300

    rupdate

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-import sys
-sys.path.append('/home/kalinin/PycharmProjects/mipt/scr')
-from scr.linearplot import *
-from scr.data import *
-from scr.gui import *
+from scr.linearplot import linear_plot
+from scr.data import Data, Errors
+from scr.gui import Gui
 
-# Создаём интерфейс
-gui = Gui()
-gui.root.mainloop()
+def main():
+    # Создаём интерфейс
+    gui = Gui()
+    gui.root.mainloop()
 
-# Импортируем дату
-data = gui.get_data()
+    # Импортируем дату
+    data = gui.get_data()
 
-# Обрабатываем данные
-errors_x = Errors()
-errors_y = Errors()
-errors_x.add_val_with_sigma(data.get_val_x(), data.get_error_x())
-errors_y.add_val_with_sigma(data.get_val_y(), data.get_error_y())
+    # Обрабатываем данные
+    errors_x = Errors()
+    errors_y = Errors()
+    errors_x.add_val_with_sigma(data.get_val_x(), data.get_error_x())
+    errors_y.add_val_with_sigma(data.get_val_y(), data.get_error_y())
 
-data.add_list_errors(errors_x.get_error(), errors_y.get_error())
+    data.add_list_errors(errors_x.get_error(), errors_y.get_error())
 
-# Строим график
-linear_plot(data.get_pair_val(), data.get_pair_error())
+    # Строим график
+    linear_plot(data.get_pair_val(), data.get_pair_error())
+
+if __name__ == "__main__":
+    main()
--- scr/gui.py
+++ scr/gui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
-from data import *
+from scr.data import Data
 
 class Gui:
     """
